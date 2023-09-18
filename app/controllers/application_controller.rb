865c0c7d971b9c8c12e0c1ab9b46c5a40a5aebb8
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-09-18 20:47:40 +0500

    configure the devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_user!
+
+    def after_sign_in_path_for(resource)
+        groups_path
+    end
+  
+    private
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    end
+  end
+  
