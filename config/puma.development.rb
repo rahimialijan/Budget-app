2233f6fd354322f39374684a245e06ed0fcde21c
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-09-21 00:59:16 +0500

    update the puma

--- config/puma.development.rb
+++
@@ -1,8 +0,0 @@
-# config/puma.development.rb
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
-
-port ENV.fetch("PORT") { 3000 }
-
-environment ENV.fetch("RAILS_ENV") { "development" }
